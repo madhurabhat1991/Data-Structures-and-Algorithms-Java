13f48c1ba4782a0cc8c6261f5c0a11f76e774694
Madhura Bhat <>
2021-05-21 16:13:30 +0000

    O(n^2)

---
+++ big_o/_04_logAllPairsOfArray.java
@@ -0,0 +1,18 @@
+class _04_logAllPairsOfArray{
+  public static void main(String[] args){
+    String[] boxes = new String[] {"a", "b", "c", "d", "e"};
+
+    logAllPairsOfArray(boxes);
+  }
+
+  public static void logAllPairsOfArray(String[] boxes){
+    for (int i = 0; i < boxes.length; ++i){ // O(n)
+      for (int j = 0; j < boxes.length; ++j){ // O(n) 
+        System.out.println(boxes[i] + " " + boxes[j]);
+      }
+    }
+
+    // Big O (n * n) = O(n^2)
+  }
+
+}
