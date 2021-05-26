b61f7dd3d2846be64b279e277b518ab125bd728c
Madhura Bhat <>
2021-05-26 19:02:35 +0000

    Hash Table implementation

---
+++ data_structures/hash_tables/_01_hashTable_implementation.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class _01_hashTable_implementation{
+  public static void main(String[] args){
+    HashTable myHash = new HashTable(50);
+    myHash.set("grapes", 1200);
+    myHash.set("apples", 9);
+    System.out.println("grapes - " + myHash.get("grapes"));
+    myHash.set("bananas", 45);
+    System.out.println("apples - " + myHash.get("apples"));
+    System.out.println("bananas - " + myHash.get("bananas"));
+  }
+}
+
+public class HashTable{
+  ArrayList<KeyValue>[] data;
+
+  public HashTable(int size){
+    this.data = new ArrayList[size];
+  }
+
+  private int _hash(String key){
+    int hash = 0;
+    for (int i =0; i < key.length(); i++){
+        hash = (hash + key.codePointAt(i) * i) % data.length;
+    }
+    return hash;
+  }
+
+  public void set(String key, int value){
+    int address = _hash(key);
+    if (data[address] == null){
+      ArrayList<KeyValue> arrayAtAddress = new ArrayList<>();
+      data[address] = arrayAtAddress;
+    }
+    KeyValue pair = new KeyValue(key, value);
+    data[address].add(pair);
+
+    KeyValue kvp = data[address].get(data[address].size()-1);
+    System.out.println(kvp.getKey() + " " + kvp.getValue());
+  }
+
+  public Integer get(String key){
+    int address = _hash(key);
+    ArrayList<KeyValue> bucket = data[address];
+    if (bucket != null){
+      for (KeyValue kvp : bucket){
+        if (kvp.getKey().equals(key)){
+          return kvp.getValue();
+        }
+      }
+    }
+    return null;
+  }
+}
+
+public class KeyValue{
+  private String key;
+  private int value;
+
+  public KeyValue(String key, int value){
+    this.key = key;
+    this.value = value;
+  }
+
+  public String getKey(){
+    return key;
+  }
+
+  public int getValue(){
+    return value;
+  }
+}
