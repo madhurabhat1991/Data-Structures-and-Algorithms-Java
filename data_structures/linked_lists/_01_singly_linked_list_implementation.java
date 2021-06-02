d34dfe350601bf18e239763bc20d50ba667d7f60
Madhura Bhat <>
2021-06-02 05:23:42 +0000

    Singly Linked list implementation

--- data_structures/linked_lists/_01_singly_linked_list_implementation.java
+++ data_structures/linked_lists/_01_singly_linked_list_implementation.java
@@ -45,6 +45,10 @@ class _01_singly_linked_list_implementation{
     myLinkedList.insert(-5, -100);  // insert anywhere before index 0 - prepend
     myLinkedList.insert(8, 100);    // insert anywhere after index 5 - append
     System.out.println(myLinkedList.getLength());
+
+    LinkedList reverseList = myLinkedList.reverse(myLinkedList);
+    reverseList.displayList();
+    myLinkedList.displayList();
   }  
 }
 
@@ -121,18 +125,31 @@ public class LinkedList{
     displayList();
   }
 
+  public LinkedList reverse(LinkedList linkedList){
+    LinkedList reverseList = new LinkedList(linkedList.head.value);
+    Node current = linkedList.head;
+    while (current.next != null){
+      current = current.next;
+      Node nextNode = new Node(current.value);
+      nextNode.next = reverseList.head;
+      reverseList.head = nextNode;
+      reverseList.length++;
+    }
+    return reverseList;
+  }
+
   public int getLength(){
     return this.length;
   }
 
   public void displayList(){
     List<Integer> myList = new ArrayList<>();
-    Node current = head;
+    Node current = this.head;
     while(current != null){
       myList.add(current.value);
       current = current.next;
     }
-    System.out.println("Head " + head.value + "\tTail " + tail.value + "\tList " + Arrays.toString(myList.toArray()));
+    System.out.println("Head " + this.head.value + "\tTail " + this.tail.value + "\tList " + Arrays.toString(myList.toArray()));
   }
 }
 
