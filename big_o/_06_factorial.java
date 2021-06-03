e7bf3f2ef54c62b7410e1586753153e27747b5d0
Madhura Bhat <>
2021-06-03 23:47:08 +0000

    O(n!)

--- big_o/_06_factorial.java
+++ big_o/_06_factorial.java
@@ -1,14 +1,14 @@
-class _06_factorial{
-  public static void main(String[] args){
+class _06_factorial {
+  public static void main(String[] args) {
     int n = 10;
 
     factorial(n);
   }
 
-  public static void factorial(int n){
-    for (int i = 0; i < n; ++i){
+  public static void factorial(int n) {
+    for (int i = 0; i < n; ++i) {
       System.out.println(n);
-      factorial(n-1);
+      factorial(n - 1);
     }
 
     // O(n!)
