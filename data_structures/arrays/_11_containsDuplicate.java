4466888ac9ecb8e816cc521c8e6673d8f2f29aad
Madhura Bhat <>
2021-06-03 23:54:11 +0000

    Contains Duplicate

--- data_structures/arrays/_11_containsDuplicate.java
+++ data_structures/arrays/_11_containsDuplicate.java
@@ -1,19 +1,21 @@
+
 // https://leetcode.com/problems/contains-duplicate/description/
 
 import java.util.*;
 
-class _11_containsDuplicate{
-  public static void main(String[] args){
-    System.out.println(containsDuplicate(new int[]{1,2,3,1}));
-    System.out.println(containsDuplicate(new int[]{1,2,3,4}));
-    System.out.println(containsDuplicate(new int[]{1}));
-    System.out.println(containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+class _11_containsDuplicate {
+  public static void main(String[] args) {
+    System.out.println(containsDuplicate(new int[] { 1, 2, 3, 1 }));
+    System.out.println(containsDuplicate(new int[] { 1, 2, 3, 4 }));
+    System.out.println(containsDuplicate(new int[] { 1 }));
+    System.out.println(containsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
   }
+
   public static boolean containsDuplicate(int[] nums) {
     Set<Integer> collection = new HashSet<>(nums.length);
 
-    for (int i = 0; i < nums.length; ++i){
-      if (collection.contains(nums[i])){
+    for (int i = 0; i < nums.length; ++i) {
+      if (collection.contains(nums[i])) {
         return true;
       }
       collection.add(nums[i]);
