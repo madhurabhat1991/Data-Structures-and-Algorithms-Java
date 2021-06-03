81fbd35484bc67d747953777cb775f55afbda9c5
Madhura Bhat <>
2021-06-03 23:49:02 +0000

    Google interview

--- problem_solving/_02_hasPairWithSum.java
+++ problem_solving/_02_hasPairWithSum.java
@@ -1,10 +1,11 @@
+
 // Given collection of numbers and sum, find matching pairs from collections that equals the sum. Return true or false if numbers adding up to sum found.
 
 import java.util.*;
 
-class _02_hasPairWithSum{
-  public static void main(String[] args){
-    int[] numbers = {6,4,3,2,1,7};
+class _02_hasPairWithSum {
+  public static void main(String[] args) {
+    int[] numbers = { 6, 4, 3, 2, 1, 7 };
     int sum = 12;
 
     boolean result = hasPairWithSumNaive(numbers, sum);
@@ -15,10 +16,10 @@ class _02_hasPairWithSum{
 
   }
 
-  public static boolean hasPairWithSumNaive(int[] numbers, int sum){
-    for (int i = 0; i < numbers.length; ++i){
-      for (int j = i + 1; j < numbers.length; ++j){
-        if (numbers[i] + numbers[j] == sum){
+  public static boolean hasPairWithSumNaive(int[] numbers, int sum) {
+    for (int i = 0; i < numbers.length; ++i) {
+      for (int j = i + 1; j < numbers.length; ++j) {
+        if (numbers[i] + numbers[j] == sum) {
           return true;
         }
       }
@@ -29,11 +30,11 @@ class _02_hasPairWithSum{
     // Space - O(1)
   }
 
-  public static boolean hasPairWithSum(int[] numbers, int sum){
+  public static boolean hasPairWithSum(int[] numbers, int sum) {
     List<Integer> comp = new ArrayList<Integer>();
 
-    for (int i = 0; i < numbers.length; ++i){
-      if (comp.contains(numbers[i])){
+    for (int i = 0; i < numbers.length; ++i) {
+      if (comp.contains(numbers[i])) {
         return true;
       }
       comp.add(sum - numbers[i]);
