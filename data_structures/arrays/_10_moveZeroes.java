26b813b00fd7f8394b70ecfdc9cbd7c45a627be8
Madhura Bhat <>
2021-06-03 23:53:38 +0000

    Move Zeroes

--- data_structures/arrays/_10_moveZeroes.java
+++ data_structures/arrays/_10_moveZeroes.java
@@ -1,25 +1,26 @@
+
 // https://leetcode.com/problems/move-zeroes/
 
 import java.util.*;
 
-class _10_moveZeroes{
-  public static void main(String[] args){
-    moveZeroes(new int[]{0,1,0,3,12});
-    moveZeroes(new int[]{0});
-    moveZeroes(new int[]{0,0,0,0,0,1});
-    moveZeroes(new int[]{1,2,4,0,5,-4,0,0,0,7,0});
+class _10_moveZeroes {
+  public static void main(String[] args) {
+    moveZeroes(new int[] { 0, 1, 0, 3, 12 });
+    moveZeroes(new int[] { 0 });
+    moveZeroes(new int[] { 0, 0, 0, 0, 0, 1 });
+    moveZeroes(new int[] { 1, 2, 4, 0, 5, -4, 0, 0, 0, 7, 0 });
   }
 
   public static void moveZeroes(int[] nums) {
     int lastIndex = 0;
 
-    for (int i = 0; i < nums.length; ++i){
-      if (nums[i] != 0){
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] != 0) {
         nums[lastIndex++] = nums[i];
       }
     }
 
-    for (int i = lastIndex; i < nums.length; ++i){
+    for (int i = lastIndex; i < nums.length; ++i) {
       nums[i] = 0;
     }
 
