4d3a4bc4870b5ed64dbcf19ed283e4209ebbd448
Madhura Bhat <>
2021-06-03 23:41:53 +0000

    O(a+b)

--- big_o/_03_compressTwoBoxes.java
+++ big_o/_03_compressTwoBoxes.java
@@ -1,17 +1,17 @@
-class _03_compressTwoBoxes{
-  public static void main(String[] args){
-    String[] boxes1 = new String[] {"a", "b", "c", "d", "e"};
-    String[] boxes2 = new String[] {"u", "v", "w", "x", "y", "z"};
+class _03_compressTwoBoxes {
+  public static void main(String[] args) {
+    String[] boxes1 = new String[] { "a", "b", "c", "d", "e" };
+    String[] boxes2 = new String[] { "u", "v", "w", "x", "y", "z" };
 
     compressTwoBoxes(boxes1, boxes2);
   }
 
-  public static void compressTwoBoxes(String[] boxes1, String[] boxes2){
-    for (String box : boxes1){ // O(a)
+  public static void compressTwoBoxes(String[] boxes1, String[] boxes2) {
+    for (String box : boxes1) { // O(a)
       System.out.println(box);
     }
 
-    for (String box : boxes2){ // O(b)
+    for (String box : boxes2) { // O(b)
       System.out.println(box);
     }
 
