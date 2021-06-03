dddb8d26967182a3f5726c6395a52d5b31c7b897
Madhura Bhat <>
2021-06-03 23:47:37 +0000

    Time complexity

--- big_o/_08_twitter.java
+++ big_o/_08_twitter.java
@@ -1,29 +1,26 @@
-public class _08_twitter{
-  public static void main(String[] args){
-    String[] tweets = new String[]{"hi","good","morning"};
+public class _08_twitter {
+  public static void main(String[] args) {
+    String[] tweets = new String[] { "hi", "good", "morning" };
 
     findTweet(tweets);
 
-    TweetObject[] tweetObjects = new TweetObject[]{
-      new TweetObject("hi", 2012),
-      new TweetObject("good", 2014),
-      new TweetObject("morning", 2018)
-    };
+    TweetObject[] tweetObjects = new TweetObject[] { new TweetObject("hi", 2012), new TweetObject("good", 2014),
+        new TweetObject("morning", 2018) };
 
     compareAllDates(tweetObjects);
   }
 
-  public static void findTweet(String[] tweets){
+  public static void findTweet(String[] tweets) {
     System.out.println("First: " + tweets[0]);
-    System.out.println("Latest: " + tweets[tweets.length-1]);
+    System.out.println("Latest: " + tweets[tweets.length - 1]);
 
     // Both O(1)
   }
 
-  public static void compareAllDates(TweetObject[] tweetObjects){
-    for (TweetObject each : tweetObjects){
-      for (TweetObject other : tweetObjects){
-        if (each.getDate() < other.getDate()){
+  public static void compareAllDates(TweetObject[] tweetObjects) {
+    for (TweetObject each : tweetObjects) {
+      for (TweetObject other : tweetObjects) {
+        if (each.getDate() < other.getDate()) {
           System.out.println(each.getDate() + " < " + other.getDate());
         }
       }
@@ -33,20 +30,20 @@ public class _08_twitter{
   }
 }
 
-class TweetObject{
+class TweetObject {
   public String Tweet;
   public int Date;
 
-  public TweetObject(String tweet, int date){
+  public TweetObject(String tweet, int date) {
     Tweet = tweet;
     Date = date;
   }
 
-  public String getTweet(){
+  public String getTweet() {
     return Tweet;
   }
 
-  public int getDate(){
+  public int getDate() {
     return Date;
   }
 }
