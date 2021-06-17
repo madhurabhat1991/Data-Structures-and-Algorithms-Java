9dd592b9a78aefa7485026fa1831311907be8291
Madhura Bhat <70220146+madhurabhat1991@users.noreply.github.com>
2021-06-17 06:44:47 +0000

    Trie implementation

--- data_structures/trees/_03_trie.java
+++ data_structures/trees/_03_trie.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-class _03_trie{
-  public static void main(String[] args){
+class _03_trie {
+  public static void main(String[] args) {
     Trie trie = new Trie();
     trie.insert("heating");
     trie.insert("heat");
@@ -12,19 +12,20 @@ class _03_trie{
   }
 }
 
-public class Trie{
+public class Trie {
   Node root;
 
-  public Trie(){
+  public Trie() {
     root = new Node();
   }
 
-  public void insert(String word){
+  public void insert(String word) {
     Node current = root;
-    /* for each character in the word check if that character exists
-    in hashmap, if not then add the character as a key in the map
-    and a new node as its value. Do this until all characters are
-    traversed. */
+    /*
+     * for each character in the word check if that character exists in hashmap, if
+     * not then add the character as a key in the map and a new node as its value.
+     * Do this until all characters are traversed.
+     */
     for (char ch : word.toCharArray()) {
       current.children.putIfAbsent(ch, new Node());
       current = current.children.get(ch);
@@ -34,48 +35,48 @@ public class Trie{
 
   // check whether a word is present in the tree.
   public boolean findWord(String string) {
-      Node current = root;
-      for (char ch : string.toCharArray()) {
-          if (current.children.containsKey(ch)) {
-              current = current.children.get(ch);
-          } else {
-              return false;
-          }
+    Node current = root;
+    for (char ch : string.toCharArray()) {
+      if (current.children.containsKey(ch)) {
+        current = current.children.get(ch);
+      } else {
+        return false;
       }
-      return current.isWord; // true;
+    }
+    return current.isWord; // true;
   }
 
   public boolean delete(String word) {
-        Node current = root;
-        Node deleteAfter = root;
-        char ch1 = word.charAt(0); // char '\0' = NULL
+    Node current = root;
+    Node deleteAfter = root;
+    char ch1 = word.charAt(0); // char '\0' = NULL
 
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            if (current.children.containsKey(ch)) {
-                current = current.children.get(ch);
-                if (current.children.size() > 1) {
-                    deleteAfter = current;
-                    ch1 = word.charAt(i + 1);
-                }
-            } else {
-                return false;
-            }
+    for (int i = 0; i < word.length(); i++) {
+      char ch = word.charAt(i);
+      if (current.children.containsKey(ch)) {
+        current = current.children.get(ch);
+        if (current.children.size() > 1) {
+          deleteAfter = current;
+          ch1 = word.charAt(i + 1);
         }
-
-        if (current.children.isEmpty()) {
-            deleteAfter.children.remove(ch1);
-            return true;
-        }
+      } else {
         return false;
+      }
     }
+
+    if (current.children.isEmpty()) {
+      deleteAfter.children.remove(ch1);
+      return true;
+    }
+    return false;
+  }
 }
 
-public class Node{
+public class Node {
   HashMap<Character, Node> children;
   boolean isWord;
 
-  public Node(){
+  public Node() {
     children = new HashMap<>();
     isWord = false;
   }
