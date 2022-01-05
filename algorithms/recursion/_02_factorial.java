7b26749cb89e0a782aac88594e92f9e8266db544
Madhura Bhat <70220146+madhurabhat1991@users.noreply.github.com>
2022-01-05 06:09:05 +0000

    Factorial

--- algorithms/recursion/_02_factorial.java
+++ algorithms/recursion/_02_factorial.java
@@ -1,5 +1,7 @@
 // Write two functions that finds the factorial of any number.
 // One should use recursive, the other should just use a for loop
+// ex: 5! = 5 * 4 * 3 * 2 * 1
+// or 5! = 5 * 4! and so on
 
 import java.util.*;
 
